74de01a1295d6931cf3ea28f836142956ef634bf
Ben Rajs Wagner <brajswagner@gmail.com>
2016-01-29 21:05:18 -0500

    Added test scene

---
+++ Assets/Scripts/View/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.W)) {
+            // Send signal to dance battle system
+
+        }
+    }
+}
