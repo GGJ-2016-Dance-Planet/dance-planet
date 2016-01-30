2891019d0263032433f15a0ba790c011220a1a93
textrivers <jon.p.woodward@gmail.com>
2016-01-30 17:57:36 -0500

    Added animations for aliens 1 and 2. Also health and platform height code

--- Assets/Scripts/View/Health.cs
+++ Assets/Scripts/View/Health.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class Health : MonoBehaviour {
@@ -14,24 +15,48 @@ public class Health : MonoBehaviour {
     }
     public float maxHealth;
 
-    public void resultHappened(Ratings rating) {
-        if (rating == Ratings.BAD) {
-            HealthStatus -= 0.1f;
-        }
+    // declaring death delegate
+    public delegate void playerdead();
+
+    // declaring death event
+    public event playerdead DeathEvent;
+
+    // declaring health change delegate
+    public delegate void healthupdate(float currentHealth);
+
+    // declaring health change event
+    public event healthupdate HealthUpdate;
+
+
+    void Start()
+    {
+        InputManager.Instance.OnRating += resultHappened;
     }
 
-	// Update is called once per frame
-	void Update () {
-        if (health <=0)
+    public void resultHappened(Ratings rating) {
+         
+        if (rating == Ratings.BAD)
         {
-            PlayerDie();
+            HealthStatus -= 0.1f;
+        }
+        if (rating == Ratings.PERFECT)
+        {
+            HealthStatus += 0.1f;
+        }
+
+        HealthUpdate(health);
+        	
+        if (HealthStatus <=0)
+        {
+            // player death event
+            if (DeathEvent != null)
+            {
+                DeathEvent();
+            }
         }
 	}
 
-    void PlayerDie()
-    {
-        
+    
 
-    }
+} 
 
-}
