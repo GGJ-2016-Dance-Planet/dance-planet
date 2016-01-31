a509fadc29509c12d60845b077bbc7fbf2a53b03
textrivers <jon.p.woodward@gmail.com>
2016-01-31 13:54:15 -0500

    lava death stuff
    lavavava

---
+++ Assets/Scripts/View/LavaDeath.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaDeath : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        // player disappears
+        Destroy(other.gameObject);
+
+        // lava splash
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        ps.Play();
+        
+        
+    }
+}
