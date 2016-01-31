641d6c20305691a288d0f4cfb8ee815c2e756901
Ben Rajs Wagner <brajswagner@gmail.com>
2016-01-31 13:27:03 -0500

    added ggj logo to title screen

---
+++ Assets/WaitToAppear.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitToAppear : MonoBehaviour {
+
+    public float wait;
+    public float fadeTime;
+
+	void Start () {
+        this.GetComponent<SpriteRenderer> ().color = Color.clear;
+        StartCoroutine (DoThing ());
+	}
+
+    IEnumerator DoThing() {
+        yield return new WaitForSeconds (wait);
+        yield return StartCoroutine(CoUtils.Interpolate(fadeTime, (t) => {
+            this.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.clear, Color.white, t/1.5f);
+        }));
+    }
+}
